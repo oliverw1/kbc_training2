89afadd1dc1a8f8835a105956ebcef4ad640aac6
Oliver Willekens <oliver.willekens@dataminded.be>
2019-05-14 13:05:27 +0200

    new content

---
+++ exercises/spark_demo/write_data.py
@@ -0,0 +1,24 @@
+"""Demo-ing the Spark DataFrame API.
+"""
+
+from pathlib import Path
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+spark = SparkSession.builder.getOrCreate()
+
+csv_file_path = Path(__file__).parents[1] / "resources" / "foo.csv"
+
+
+frame = (spark
+         .read
+         .options(header="true", sep=";")
+         .csv(str(csv_file_path))
+         )
+
+frame.printSchema()
+
+better_frame = frame.withColumn("Date_of_Birth", F.to_date(F.col("Date_of_Birth")))
+better_frame.printSchema()
+better_frame.write.orc(str(csv_file_path.parent / "as_parquet"))
